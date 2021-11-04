841a943273e71f1ba1d62a5d975fc64a0719f101
“jungkeunmo“ <abc59684495@gmail.com>
2021-11-04 23:22:19 +0900

    parc

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -20,4 +20,39 @@ router.get("/signup", checkLogin,(req, res, next) => {
     res.render("screens/signup", {loggedIn});
 });
 
+router.post("/signup", (req, res, next) => {
+    console.log(req.body.input_mobile);
+    console.log(req.body.input_password);
+    console.log(req.body.input_email);
+    console.log(req.body.input_name);
+
+    const insertQuery = `
+        INSERT INTO users (
+            email,
+            password,
+            name,
+            mobile,
+            createdAt,
+            updatedAt
+        ) VALUES (
+            "${req.body.input_email}",
+            "${req.body.input_password}",
+            "${req.body.input_name}",
+            "${req.body.input_mobile}",
+            now(),
+            now()
+        )
+    `;
+    try {
+        db.query(insertQuery, (err, rows) => {
+            if (!err) {
+                return res.redirect("/");
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send("회원가입에 실패했습니다.");
+    }
+});
+
 module.exports = router;
